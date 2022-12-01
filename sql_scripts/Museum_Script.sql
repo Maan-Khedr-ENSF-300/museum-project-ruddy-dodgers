912ed6603fcf85c7b942bee036c9ad4f9af1500d
NickSavino <77707655+NickSavino@users.noreply.github.com>
2022-12-01 14:18:27 -0700

    Refactoring of Model

--- sql_scripts/Museum_Script.sql
+++ sql_scripts/Museum_Script.sql
@@ -9,11 +9,11 @@ CREATE TABLE ART_OBJECT (
     Style			varchar(30) not null,
     Title			varchar(50) not null,
     Year_C			varchar(30) not null,
-    Description		varchar(30) not null,
+    AODescription	varchar(30) not null,
 	primary key (Unique_ID)
 );
 
-INSERT INTO ART_OBJECT (Unique_ID, Style, Title, Year_C, Description)
+INSERT INTO ART_OBJECT (Unique_ID, Style, Title, Year_C, AODescription)
 VALUES
 (001, "Renaissance", "Angel Bearing Candlestick", "1524-29"),
 (002, "Late Medieval", "Field Armor of King Henry VIII", "1509-47"),
@@ -33,7 +33,7 @@ CREATE TABLE PAINTING (
 );
 
 DROP TABLE IF EXISTS SCULPTURE_STATUE;
-CREATE TABLE ART_OBJECT (
+CREATE TABLE SCULPTURE_STATUE (
 	Unique_ID		int not null,
     Weight			varchar(30) not null,
     Height			varchar(30) not null,
@@ -45,25 +45,26 @@ CREATE TABLE ART_OBJECT (
 DROP TABLE IF EXISTS OTHER;
 CREATE TABLE OTHER (
 	Unique_ID		int not null,
-    Type			varchar(30) not null,
+    OType			varchar(30) not null,
 	primary key (Unique_ID),
     foreign key (Unique_ID) references ART_OBJECT(Unique_ID)
 );
 
+DROP TABLE IF EXISTS ARTIST;
 CREATE TABLE ARTIST (
 	Unique_ID			int not null,
-    Name				varchar(30),
+    AName				varchar(30),
     Date_Born			varchar(30),
     Date_Died			varchar(30),
     Main_Style			varchar(30),
     Epoch				varchar(30),
     Origin_Country		varchar(30),
-    Description			varchar(30),
+    ADescription		varchar(30),
 	primary key (Name),
     foreign key (Unique_ID) references ART_OBJECT(Unique_ID)
 );
 
-INSERT INTO ARTIST (Unique_ID, Name, Date_Born, Date_Died, Main_Style, Epoch, Origin_Country, Description)
+INSERT INTO ARTIST (Unique_ID, Name, Date_Born, Date_Died, Main_Style, Epoch, Origin_Country, ADescription)
 VALUES
 (001, "Benedetto da Rovezzano", "1474", "1554"),
 (002),
@@ -72,15 +73,15 @@ VALUES
 (005, "Affabel Partridge", "1551", "1580"),
 (006, "Affabel Partridge");
 
-
+DROP TABLE IF EXISTS COLLECTIONS;
 CREATE TABLE COLLECTIONS (
 	Unique_ID			int not null,
-    Name				varchar(30) not null,
+    CName				varchar(30) not null,
     Contact_Person		varchar(30) not null,
     Phone				varchar(30) not null,
     Address				varchar(30) not null,
-    Type				varchar(30),
-    Description			varchar(30),
+    CType				varchar(30),
+    CDescription		varchar(30),
 	primary key (Name),
     foreign key (Unique_ID) references ART_OBJECT(Unique_ID)
 );
