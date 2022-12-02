e60d3c7377b6f2875b23c9e947254c1703588817
NicolaSavinoUCalgary <Nicola.Savino1@Ucalgary.ca>
2022-12-01 17:34:24 -0700

    Addded User Privileges and more tables to schema

--- sql_scripts/Museum_Script.sql
+++ sql_scripts/Museum_Script.sql
@@ -9,7 +9,7 @@ CREATE TABLE ART_OBJECT (
     Style			varchar(30) not null,
     Title			varchar(50) not null,
     Year_C			varchar(30) not null,
-    AODescription	varchar(30) not null,
+    AODescription	varchar(30),
 	primary key (Unique_ID)
 );
 
@@ -50,6 +50,26 @@ CREATE TABLE OTHER (
     foreign key (Unique_ID) references ART_OBJECT(Unique_ID)
 );
 
+DROP TABLE IF EXISTS PERMANENT_COLLECTION;
+CREATE TABLE PERMANENT_COLLECTION (
+	Unique_ID				int not null,
+    Date_acquired			varchar(30) not null,
+    Collection_Status		varchar(30) not null,
+    Cost					varchar(30) not null,
+	primary key (Unique_ID),
+    foreign key (Unique_ID) references ART_OBJECT(Unique_ID)
+);
+
+DROP TABLE IF EXISTS BORROWED;
+CREATE TABLE BORROWED (
+	Unique_ID		int not null,
+    Borrowed_from			varchar(30) not null,
+    Date_borrowed			varchar(30) not null,
+    Date_returned			varchar(30) not null,
+	primary key (Unique_ID),
+    foreign key (Unique_ID) references ART_OBJECT(Unique_ID)
+);
+
 DROP TABLE IF EXISTS ARTIST;
 CREATE TABLE ARTIST (
 	Unique_ID			int not null,
@@ -76,15 +96,54 @@ VALUES
 DROP TABLE IF EXISTS COLLECTIONS;
 CREATE TABLE COLLECTIONS (
 	Unique_ID			int not null,
-    CName				varchar(30) not null,
+    Cname				varchar(30) not null,
     Contact_Person		varchar(30) not null,
     Phone				varchar(30) not null,
     Address				varchar(30) not null,
     CType				varchar(30),
     CDescription		varchar(30),
-	primary key (Name),
+	primary key (Cname),
     foreign key (Unique_ID) references ART_OBJECT(Unique_ID)
 );
 
+DROP TABLE IF EXISTS BACKGROUND;
+CREATE TABLE BACKGROUND (
+	Unique_ID		int not null,
+    Origin				varchar(30) not null,
+    Attribute			varchar(30) not null,
+	primary key (Unique_ID),
+    foreign key (Unique_ID) references ART_OBJECT(Unique_ID)
+);
 
+DROP TABLE IF EXISTS EXHIBITIONS;
+CREATE TABLE EXHIBITIONS (
+	Unique_ID		int not null,
+    Ename				varchar(30) not null,
+    Start_date			varchar(30) not null,
+    End_date			varchar(30) not null,
+	primary key (Unique_ID),
+    foreign key (Unique_ID) references ART_OBJECT(Unique_ID)
+);
+
+DROP ROLE IF EXISTS db_admin@localhost, read_access@localhost;
+CREATE ROLE db_admin@localhost, data_access@localhost, read_access@localhost;
+GRANT ALL PRIVILEGES ON MUSEUMART.* TO db_admin@localhost;
+GRANT Insert ON MUSEUMART.* TO data_access@localhost;
+GRANT Select ON MUSEUMART.* TO read_access@localhost;
+
+DROP USER IF EXISTS administrator@localhost;
+DROP USER IF EXISTS data_entry_user@localhost;
+DROP USER IF EXISTS guest@localhost;
+
+CREATE USER administrator@localhost IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE USER data_entry_user@localhost IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE USER guest@localhost;
+
+GRANT db_admin@localhost TO administrator@localhost;
+GRANT data_access@localhost TO data_entry_user@localhost;
+GRANT read_access@localhost TO guest@localhost;
+
+SET DEFAULT ROLE ALL TO administrator@localhost;
+SET DEFAULT ROLE ALL TO data_entry_user@localhost;
+SET DEFAULT ROLE ALL TO guest@localhost;
 
